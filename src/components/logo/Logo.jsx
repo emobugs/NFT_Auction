25afbebd6ff84396b9c922c3cdf0ee8625c13f10
emo_new_bug <emotupev11@gmail.com>
2023-02-14 11:48:45 +0200

    fix: some program logic

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,17 +1,28 @@
-import logo from "../../../public/images/logo.svg"
-import logoMuted from "../../../public/images/logo-muted.svg"
 
-export default function Logo ({type = 'default'}) {
-    
-    return(
-        <div>
-            {type === 'default' && 
-            <img src={logo.src} alt="default logo" /> }
-            {type === 'muted' && 
+import logoPath from "../../../public/images/logo.svg"
+import logoMutedPath from "../../../public/images/logo-muted.svg"
+import { useState, useEffect } from "react";
+
+export default function Logo({ type = "default" }) {
+  const [logo, setlogo] = useState(logo);
+
+  useEffect(() => {
+    if(type === 'default'){
+        setlogo(logoPath.src)
+    } else if(type === 'muted'){
+        setlogo(logoMutedPath.src)
+    }
+}, [type]);
+
+  return (
+    <div>
+      {<img src={logo} alt="logo" /> }
+      {/* {type === 'muted' && <img src={logo} alt="logo" /> } */}
+      {/* {type === 'muted' && 
             <img src={logoMuted.src} alt="grayed logo" />
-            }
-            {/* {<img src={type === 'muted' ? logoMuted.src : logo.src} alt="setaa" />
             } */}
-        </div>
-    )
+      {/* {<img src={type === 'muted' ? logoMuted.src : logo.src} alt="setaa" />
+            } */}
+    </div>
+  );
 }
