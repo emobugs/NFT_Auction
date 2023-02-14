85a8613835a17343e82d2af4a01e66e58039161b
emo_new_bug <emotupev11@gmail.com>
2023-02-14 12:07:33 +0200

    fix: use relative path for the images

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,28 +1,14 @@
 
-import logoPath from "../../../public/images/logo.svg"
-import logoMutedPath from "../../../public/images/logo-muted.svg"
-import { useState, useEffect } from "react";
+import logo from "../../../public/images/logo.svg"
+import logoMuted from "../../../public/images/logo-muted.svg"
+// import { useState, useEffect } from "react";
 
 export default function Logo({ type = "default" }) {
-  const [logo, setlogo] = useState(logo);
-
-  useEffect(() => {
-    if(type === 'default'){
-        setlogo(logoPath.src)
-    } else if(type === 'muted'){
-        setlogo(logoMutedPath.src)
-    }
-}, [type]);
 
   return (
     <div>
-      {<img src={logo} alt="logo" /> }
-      {/* {type === 'muted' && <img src={logo} alt="logo" /> } */}
-      {/* {type === 'muted' && 
-            <img src={logoMuted.src} alt="grayed logo" />
-            } */}
-      {/* {<img src={type === 'muted' ? logoMuted.src : logo.src} alt="setaa" />
-            } */}
+      {type === 'default' && <img src='./images/logo.svg' alt="logo" /> }
+      { type === 'muted' && <img src='./images/logo-muted.svg' alt="grayed logo" />}
     </div>
   );
 }
