80f8aee0fc5a8b624d15aed4cdd610203403635a
emo_new_bug <emotupev11@gmail.com>
2023-02-14 12:44:33 +0200

    fix: change something

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,11 +1,8 @@
 
-export default function Logo({ type = "default" }) {
-    const defaultUrl = `./images/logo.svg`;
-    const mutedtUrl = `./images/logo-muted.svg`;
-    // const imgName = {
-    //     'default': '',
-    //     'muted': '-muted'
-    // }
+export default function Logo({ type = 'default' }) {
+    const defaultUrl = "./images/logo.svg";
+    const mutedtUrl = "./images/logo-muted.svg";
+
     if(type === 'default'){
         return <img src={defaultUrl} alt="logo" /> ;
     } return <img src={mutedtUrl} alt="logo" />
