4adb649aeb6083b795937af2dcb7f72f80c20de7
emo_new_bug <emotupev11@gmail.com>
2023-02-14 22:34:56 +0200

    feat: remove imports for testing purposes

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -4,12 +4,10 @@ import Link from "../link/Link";
 import styles from "./Example.module.scss";
 import classNames from "classnames";
 
-import User from "../user/User";
 
 export default function Copyright() {
   return (
     <div className={classNames(styles.wrapper)}>
-      <User name="terika88" info="132 items" avatar="./images/avatar.png" size="200"/>
       <Container className={classNames(styles.container)} maxWidth="xl">
         <Paper className={classNames(styles.paper)}>
           <Link href="/about" color="secondary">  
