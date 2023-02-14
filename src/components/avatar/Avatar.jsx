64b54f02ab8dbfe20213209084d6aa029593f629
emo_new_bug <emotupev11@gmail.com>
2023-02-14 21:09:24 +0200

    fix: i really don't know already

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -3,7 +3,7 @@ import styles from './Avatar.module.scss'
 export default function Avatar({size = 90, url, verified = false}){
 
     return(
-        <div className={styles.avatar} style={{width: size+'px', height: size+'px'}}>
+        <div className={styles.avatar} style={{width: size +'px', height: size +'px'}}>
             <img src={url+''} alt="" className={styles.image}/>
             <img src="images/verified.svg" alt="badge" className={styles.badge}/>
         </div>
--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -3,10 +3,12 @@ import Container from "@mui/material/Container";
 import Link from "../link/Link";
 import styles from "./Example.module.scss";
 import classNames from "classnames";
+import Avatar from "../avatar/Avatar";
 
 export default function Copyright() {
   return (
     <div className={classNames(styles.wrapper)}>
+          <Avatar size="50" url="/images/avatar.png"/>
       <Container className={classNames(styles.container)} maxWidth="xl">
         <Paper className={classNames(styles.paper)}>
           <Link href="/about" color="secondary">  
