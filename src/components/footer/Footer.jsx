2b1a03c3bdb78f7cfd7703926b9bee766b3236d1
emo_new_bug <emotupev11@gmail.com>
2023-02-16 00:43:37 +0200

    feat: add grid in the container

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,20 +7,22 @@ import { Grid, Button, Container } from "@mui/material";
 export default function Footer() {
   return (
     <div className={styles.wrapper}>
-      <Container className={styles.container} justifyContent="space-between" alignItems="center">
-        <Grid item md={4}>
-          <Logo className={styles.logo} type="muted" />
-        </Grid>
-        <Grid item md={4} className={styles.grid}>
-          <p className={styles.text}>Emo All Aights Reserved 2023</p>
-        </Grid>
-        <Grid item md={4} className={styles.grid}>
-          <Button href="#" item md={6} className={styles.button}>
-            Privacy Policy
-          </Button>
-          <Button href="#" item md={6} className={styles.button}>
-            Cookie Policy
-          </Button>
+      <Container className={styles.container}>
+        <Grid container alignItems='center'>
+          <Grid item md={4}>
+            <Logo className={styles.logo} type="muted" />
+          </Grid>
+          <Grid item md={4} className={styles.grid}>
+            <p className={styles.text}>Emo All Aights Reserved 2023</p>
+          </Grid>
+          <Grid item md={4} className={styles.grid}>
+            <Button href="#" item md={6} className={styles.button}>
+              Privacy Policy
+            </Button>
+            <Button href="#" item md={6} className={styles.button}>
+              Cookie Policy
+            </Button>
+          </Grid>
         </Grid>
       </Container>
     </div>
