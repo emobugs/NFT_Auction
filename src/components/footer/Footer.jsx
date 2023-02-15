08d8d7f99b313abc49f963a8d36f3fc6084aa58d
emo_new_bug <emotupev11@gmail.com>
2023-02-15 22:54:49 +0200

    fix: change grid container for container

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,21 +1,26 @@
-import styles from './Footer.module.scss'
+import styles from "./Footer.module.scss";
 
-import Logo from '../logo/Logo'
+import Logo from "../logo/Logo";
 
-import {Grid, Button, Container} from '@mui/material'
+import { Grid, Button, Container } from "@mui/material";
 
-export default function Footer(){
-
-    return(
-        <Grid container className={styles.container}>
-            <Grid item md={4}><Logo type="muted"/></Grid>
-            <Grid item md={4}><p className={styles.text}>Emo All Aights Reserved 2023</p></Grid>
-            <Grid item md={4}>
-                {/* <Container> */}
-                    <Button href="#" item md={6} className={styles.button}>Privacy Policy</Button>
-                    <Button href="#" item md={6} className={styles.button}>Cookie Policy</Button>
-                {/* </Container> */}
-            </Grid>
-        </Grid>
-    )
+export default function Footer() {
+  return (
+    <Container className={styles.container}>
+      <Grid item md={4}>
+        <Logo type="muted" />
+      </Grid>
+      <Grid item md={4}>
+        <p className={styles.text}>Emo All Aights Reserved 2023</p>
+      </Grid>
+      <Grid item md={4}>
+        <Button href="#" item md={6} className={styles.button}>
+          Privacy Policy
+        </Button>
+        <Button href="#" item md={6} className={styles.button}>
+          Cookie Policy
+        </Button>
+      </Grid>
+    </Container>
+  );
 }
