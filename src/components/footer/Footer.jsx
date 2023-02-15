937aad01861b0ac118511761687f601ececb92f7
emo_new_bug <emotupev11@gmail.com>
2023-02-15 23:56:51 +0200

    fix: remove container

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,22 +7,22 @@ import { Grid, Button, Container } from "@mui/material";
 export default function Footer() {
   return (
     <div className={styles.wrapper}>
-    <Container className={styles.container}>
-      <Grid item md={4}>
-        <Logo className={styles.logo} type="muted" />
+      <Grid container justifyContent="space-between" alignItems="center">
+        <Grid item md={4}>
+          <Logo className={styles.logo} type="muted" />
+        </Grid>
+        <Grid item md={4}>
+          <p className={styles.text}>Emo All Aights Reserved 2023</p>
+        </Grid>
+        <Grid item md={4}>
+          <Button href="#" item md={6} className={styles.button}>
+            Privacy Policy
+          </Button>
+          <Button href="#" item md={6} className={styles.button}>
+            Cookie Policy
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item md={4}>
-        <p className={styles.text}>Emo All Aights Reserved 2023</p>
-      </Grid>
-      <Grid container item md={4}>
-        <Button href="#" item md={6} className={styles.button}>
-          Privacy Policy
-        </Button>
-        <Button href="#" item md={6} className={styles.button}>
-          Cookie Policy
-        </Button>
-      </Grid>
-    </Container>
     </div>
   );
 }
