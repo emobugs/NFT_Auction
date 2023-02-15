4bc802bbd9c8a39b10d9bcf3e90ff85cca74ef61
emo_new_bug <emotupev11@gmail.com>
2023-02-16 00:13:03 +0200

    fix: add classes to grids elements

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,15 +11,12 @@ export default function Footer() {
         <Grid item md={4}>
           <Logo className={styles.logo} type="muted" />
         </Grid>
-        <Grid item md={4}>
+        <Grid item md={4} className={styles.grid}>
           <p className={styles.text}>Emo All Aights Reserved 2023</p>
         </Grid>
-        <Grid item md={4}>
+        <Grid item md={4} className={styles.grid}>
           <Button href="#" item md={6} className={styles.button}>
             Privacy Policy
-          </Button>
-          <Button href="#" item md={6} className={styles.button}>
-            Cookie Policy
           </Button>
           <Button href="#" item md={6} className={styles.button}>
             Cookie Policy
