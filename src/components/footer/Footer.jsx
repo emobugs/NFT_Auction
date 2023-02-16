0b148220e7205579cf400e7df31995c1917bbfbd
emo_new_bug <emotupev11@gmail.com>
2023-02-16 02:47:37 +0200

    fix: change code at whole

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,64 +1,31 @@
-import styles from "./Footer.module.scss";
-
+import React from "react";
 import Logo from "../logo/Logo";
+import { Grid, Container, Button } from "@mui/material";
 
-import { Grid, Button, Container } from "@mui/material";
+ import styles from "./Footer.module.scss";
+ import SearchIcon from '@mui/icons-material/Search';
+
 
 export default function Footer() {
   return (
-    // <div className={styles.wrapper}>
-    //   <Container className={styles.container} maxWidth="false">
-    //     <Grid container direction="row" alignItems="center" justifyContent="center">
-    //       <Grid className={styles.logoGrid} item md={4}>
-    //         <Logo className={styles.logo} type="muted" />
-    //       </Grid>
-    //       <Grid className={styles.inputGrid} item md={4}>
-    //         <p className={styles.text}>Emo All Aights Reserved 2023</p>
-    //       </Grid>
-    //       <Grid className={styles.buttonsGrid} item md={4} >
-    //         <Button className={styles.button} variant="text">
-    //           Privacy Policy
-    //         </Button>
-    //         <Button className={styles.button} variant="text">
-    //           Cookie Policy
-    //         </Button>
-    //       </Grid>
-    //     </Grid>
-    //   </Container>
-    // </div>
     <div className={styles.wrapper}>
-      <Grid container justifyContent="space-between" alignItems="center">
-        <Grid item md={2}>
-          <Logo className={styles.logo} />
-        </Grid>
-        <Grid item md={4}>
-          {/* <TextField
-            id="input-search"
-            placeholder="Find items, users and activities"
-            className={styles.TextField}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-            }}
-            variant="standard"
-          /> */}
-          <p>Copyright</p>
-        </Grid>
-        <Grid
-          container
-          item
-          md={4}
-          flexDirection="row"
-          className={styles.container}
-        >
-          <Button className={styles['btn-nav']}>Home</Button>
-          <Button className={styles['btn-nav']}>Activity</Button>
-          <Button variant="contained" className={styles['btn-nav','btn-nav-exp']}>Explore</Button>
-        </Grid>
+      <Container className={styles.container} maxWidth="false">
+    <Grid container direction="row" justifyContent="center" alignItems="center">
+      <Grid className={styles.logoGrid} item xs={4}>
+        <Logo type="muted" />
       </Grid>
+      <Grid className={styles.inputGrid} item xs={4}>
+        
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+       
+      </Grid>
+      <Grid className={styles.buttonGrid} item xs={4}>
+        <Button   className={styles.textBtn} variant="text">Privacy policy</Button>
+        <Button className={styles.textBtn}  variant="text">Cookie policy</Button>
+        
+      </Grid>
+    </Grid>
+    </Container>
     </div>
   );
 }
