3212dd18351f067bfd1fa261b60c82d7fd63fc4b
emo_new_bug <emotupev11@gmail.com>
2023-02-16 22:11:56 +0200

    feat: add Container component

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -8,30 +8,32 @@ export default function Featured({ items = [] }) {
 
   return (
     <div className={styles.wrapper}>
-      <ImageList
-        className={styles.imageList}
-        sx={{ gridAutoFlow: "column" }}
-        variant="quilted"
-        cols={3}
-        rowHeight={164}
-      >
-        {items.map((item, i) => (
-          <ImageListItem
-            className={styles.imageItem}
-            key={i+1}
-            cols={item.cols || 1}
-            rows={item.rows || 1}
-          >
-            <img
-              className={styles.image}
-              srcSet={[item.image, 120, item.rows, item.cols]}
-              alt={item.title}
-              loading="lazy"
-              onClick={() => router.push(item.image)}
-            />
-          </ImageListItem>
-        ))}
-      </ImageList>
+      <Container className={styles.container}>
+        <ImageList
+          className={styles.imageList}
+          sx={{ gridAutoFlow: "column" }}
+          variant="quilted"
+          cols={3}
+          rowHeight={164}
+        >
+          {items.map((item, i) => (
+            <ImageListItem
+              className={styles.imageItem}
+              key={i + 1}
+              cols={item.cols || 1}
+              rows={item.rows || 1}
+            >
+              <img
+                className={styles.image}
+                srcSet={[item.image, 120, item.rows, item.cols]}
+                alt={item.title}
+                loading="lazy"
+                onClick={() => router.push(item.href)}
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </Container>
     </div>
   );
 }
