1299b5f1c13ce14362dc8c249ad9087ef9707980
emo_new_bug <emotupev11@gmail.com>
2023-02-18 02:41:07 +0200

    fix: swap grid container with ordinary div

--- src/components/collectors/TopCollectors.jsx
+++ src/components/collectors/TopCollectors.jsx
@@ -3,17 +3,14 @@ import styles from "./TopCollectors.module.scss";
 import { useState, useEffect } from "react";
 
 import chunk from "lodash/chunk";
-import { Grid, FormControl, Select, MenuItem } from "@mui/material";
+import { Grid, FormControl, Select, MenuItem, Container } from "@mui/material";
 
 import Collector from "./Collector";
 import CollectorColumn from "./CollectorColumn";
-import nextId from "react-id-generator";
 
 export default function TopCollectors({ collectors }) {
   const [collectorsList, setCollectorsList] = useState();
   const [number, setNumber] = useState(1);
-
-  const reactId = nextId();
 
   const chunkCollectors = () => {
     const collectorsL = collectors.map((c, i) => {
@@ -28,15 +25,15 @@ export default function TopCollectors({ collectors }) {
       );
     });
     setCollectorsList(chunk(collectorsL, 3));
-    // return chunk(collectorsL, 3);
   };
 
   useEffect(() => {
+    collectors.map((item,index)=>item.id=index+1)
     chunkCollectors();
   }, []);
 
   return (
-      <Grid className={styles.container} container >
+      <div className={styles.container} >
         <Grid className={styles.top} container >
           <Grid item md={10}>
             <h1 className={styles.heading}>Top Collectors</h1>
@@ -68,7 +65,7 @@ export default function TopCollectors({ collectors }) {
             })
           }
         </Grid>
-      </Grid>
+      </div>
   );
 
   //   TopCollectors.PropTypes = {
