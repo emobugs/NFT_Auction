f7eea755641b7f9f5bf168af646a5dd6cf48512d
emo_new_bug <emotupev11@gmail.com>
2023-02-18 03:16:18 +0200

    fix: simplify the code

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,185 +1,91 @@
-import Example from "../src/components/example/Example";
-import Footer from "../src/components/footer/Footer";
-import Card from "../src/components/card/Card";
-import How from "../src/components/how/How";
-import Step from "../src/components/how/Step";
-// import Featured from "../src/components/featured/Featured";
-import CollectorColumn from "../src/components/collectors/CollectorColumn";
-import TopCollectors from "../src/components/collectors/TopCollectors";
+import Header from "../src/components/header/Header"
+import Featured from "../src/components/featured/Featured";
 import Trending from "../src/components/trending/Trending";
+import TopCollectors from "../src/components/collectors/TopCollectors";
+import How from "../src/components/how/How";
+import Auctions from "../src/components/auctions/Auctions";
+import Footer from "../src/components/footer/Footer"
 
 export default function Index() {
   return (
     <div>
-    <TopCollectors collectors= {
-      [
+      {/* <Header></Header>
+      <Featured></Featured>
+      <Trending></Trending>
+      <TopCollectors></TopCollectors>
+      <How></How>
+      <Auctions></Auctions> */}
+      <TopCollectors collectors={
+        [
         {
           name: 'emo',
-          nftsCount: 120,
-          avatar: './images/avatar.png',
-          verified: true,
-        },
-        {
-          name: 'ivan',
-          nftsCount: 32,
-          avatar: './images/avatar.png',
-          verified: true,
-        },
-        {
-          name: 'sasho',
-          nftsCount: 20,
+          nftsCount: 12,
           avatar: './images/avatar.png',
           verified: true,
         },
         {
           name: 'emo',
-          nftsCount: 120,
-          avatar: './images/avatar.png',
-          verified: true,
-        },
-        {
-          name: 'ivan',
-          nftsCount: 32,
-          avatar: './images/avatar.png',
-          verified: true,
-        },
-        {
-          name: 'sasho',
-          nftsCount: 20,
+          nftsCount: 12,
           avatar: './images/avatar.png',
           verified: true,
         },
         {
           name: 'emo',
-          nftsCount: 120,
+          nftsCount: 12,
           avatar: './images/avatar.png',
           verified: true,
         },
         {
-          name: 'ivan',
-          nftsCount: 32,
+          name: 'emo',
+          nftsCount: 12,
           avatar: './images/avatar.png',
           verified: true,
         },
         {
-          name: 'sasho',
-          nftsCount: 20,
+          name: 'emo',
+          nftsCount: 12,
           avatar: './images/avatar.png',
           verified: true,
         },
+        {
+          name: 'emo',
+          nftsCount: 12,
+          avatar: './images/avatar.png',
+          verified: true,
+        },
+        {
+          name: 'emo',
+          nftsCount: 12,
+          avatar: './images/avatar.png',
+          verified: true,
+        },
+        {
+          name: 'emo',
+          nftsCount: 12,
+          avatar: './images/avatar.png',
+          verified: true,
+        },
+        {
+          name: 'emo',
+          nftsCount: 12,
+          avatar: './images/avatar.png',
+          verified: true,
+        },
+        {
+          name: 'emo',
+          nftsCount: 12,
+          avatar: './images/avatar.png',
+          verified: true,
+        },
+        {
+          name: 'emo',
+          nftsCount: 12,
+          avatar: './images/avatar.png',
+          verified: true,
+        }
       ]
-    }></TopCollectors>
-      {/* <CollectorColumn
-        items={[
-          {
-            name: "emo",
-            nftsCount: 155,
-            avatar: "./images/avatar.png",
-            verified: true,
-            id: 1,
-          },
-          {
-            name: 'ivan',
-            nftsCount: 25,
-            avatar: './images/avatar.png',
-            verified: true,
-            id: 2
-          },
-          {
-            name: 'Sasho',
-            nftsCount: 6,
-            avatar: './images/avatar.png',
-            verified: true,
-            id: 3
-          }
-        ]}
-      ></CollectorColumn> */}
-      {/* <Featured items = {
-        [
-          {
-            image:
-              "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=500&h=500",
-            title: "Breakfast",
-            rows: 2,
-            cols: 3,
-            href: "/about",
-          },
-          {
-            image:
-              "https://images.unsplash.com/photo-1551782450-a2132b4ba21d?w=500&h=500",
-            title: "Burger",
-            href: "/about",
-          },
-          {
-            image:
-              "https://images.unsplash.com/photo-1522770179533-24471fcdba45?w=500&h=500",
-            title: "Camera",
-            href: "/about",
-          },
-          {
-            image:
-              "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c?w=500&h=500",
-            title: "Coffee",
-            href: "/about",
-          },
-          {
-            image:
-              "https://images.unsplash.com/photo-1533827432537-70133748f5c8?w=500&h=500",
-            title: "Hats",
-            href: "/about",
-          },
-          {
-            image:
-              "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62?w=500&h=500",
-            title: "Honey",
-            href: "/about",
-          },
-          {
-            image:
-              "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6?w=500&h=500",
-            title: "Basketball",
-            href: "/about",
-          },
-        ]
-      }></Featured> */}
-      {/* <Footer></Footer> */}
-      {/* <How items = {
-        [
-          {
-            number:1,
-            title:'Setaaa idvam za teb',
-            description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
-            quo placeat et dolore quis blanditiis unde minima minus,
-            voluptatem soluta porro numquam neque qui maxime. Id suscipit fuga
-            cum non.`
-           },
-           {
-            number:2,
-            title:'ela da vidish ti',
-            description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
-            quo placeat et dolore quis blanditiis unde minima minus,
-            voluptatem soluta porro numquam neque qui maxime. Id suscipit fuga
-            cum non.`
-           },
-           {
-            number:2,
-            title:'ela da vidish ti',
-            description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
-            quo placeat et dolore quis blanditiis unde minima minus,
-            voluptatem soluta porro numquam neque qui maxime. Id suscipit fuga
-            cum non.`
-           }
-
-        ]
-      }></How> */}
-      {/* <Step
-                    number={1}
-                    title={"ela da vidish ti"}
-                    description= {`Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
-                    quo placeat et dolore quis blanditiis unde minima minus,
-                    voluptatem soluta porro numquam neque qui maxime. Id suscipit fuga
-                    cum non.`}
-                  > </Step> */}
+      }></TopCollectors>
     </div>
+   
   );
 }
--- src/components/collectors/TopCollectors.jsx
+++ src/components/collectors/TopCollectors.jsx
@@ -1,75 +1,46 @@
 import styles from "./TopCollectors.module.scss";
 
-import { useState, useEffect } from "react";
 
 import chunk from "lodash/chunk";
 import { Grid, FormControl, Select, MenuItem, Container } from "@mui/material";
 
-import Collector from "./Collector";
 import CollectorColumn from "./CollectorColumn";
 
 export default function TopCollectors({ collectors }) {
-  const [collectorsList, setCollectorsList] = useState();
-  const [number, setNumber] = useState(1);
 
-  const chunkCollectors = () => {
-    const collectorsL = collectors?.map((c, i) => {
-      return (
-        <Collector
-          name={c.name}
-          nftsCount={c.nftsCount}
-          avatar={c.avatar}
-          verified={c.verified}
-          id={i + 1}
-        ></Collector>
-      );
-    });
-    setCollectorsList(chunk(collectorsL, 3));
-  };
-
-  useEffect(() => {
-    // collectors.map((item,index)=>item.id=index+1)
-    chunkCollectors();
-  }, []);
+  collectors.map((item, index) => (item.id = index + 1));
 
   return (
-    <Container className={styles.container}>
+    <Container className={styles.container} maxWidth={false}>
       <Grid className={styles.top} container>
-        {/* <Grid item md={10}> */}
+        <Grid item md={10}>
         <h1 className={styles.heading}>Top Collectors</h1>
-        {/* </Grid> */}
-        {/* <Grid className={styles.gridSel} item md={2}> */}
+        </Grid>
+        <Grid className={styles.gridSel} item md={2}>
         <FormControl sx={{ width: "100%", marginLeft: "auto" }}>
           <Select
             className={styles.selectEl}
             labelId="select"
             id="select"
             label="Today"
+            defaultValue="today"
           >
-            <MenuItem value={"1"}>Today</MenuItem>
-            <MenuItem value={"2"}>This Week</MenuItem>
-            <MenuItem value={"3"}>This Month</MenuItem>
+            <MenuItem value={"today"}>Today</MenuItem>
+            <MenuItem value={"week"}>This Week</MenuItem>
+            <MenuItem value={"month"}>This Month</MenuItem>
           </Select>
         </FormControl>
-        {/* </Grid> */}
+        </Grid>
       </Grid>
-      <Container>
-        <Grid container direction="row">
-          {
-            // console.log(collectorsList)
-            collectorsList?.map((innerList, i) => {
-              return (
-                <Grid className={styles.gridContainer} item md={4} key={i}>
-                  <CollectorColumn
-                    className={styles.collectorColumn}
-                    items={innerList.map((list) => {
-                      return list.props;
-                    })}
-                  ></CollectorColumn>
-                </Grid>
-              );
-            })
-          }
+      <Container maxWidth={false}>
+        <Grid className={styles.collectorsGrid} container direction="row">
+          {chunk(collectors, 3).map((collectors) => {
+            return (
+              <Grid item md={3}>
+                <CollectorColumn items={collectors}></CollectorColumn>
+              </Grid>
+            );
+          })}
         </Grid>
       </Container>
     </Container>
