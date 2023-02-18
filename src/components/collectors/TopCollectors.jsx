98a13c7b7eee0236bf58d74d0ea7eea313a17dee
emo_new_bug <emotupev11@gmail.com>
2023-02-18 03:51:05 +0200

    fix: remove more containers

--- src/components/collectors/TopCollectors.jsx
+++ src/components/collectors/TopCollectors.jsx
@@ -1,5 +1,6 @@
 import styles from "./TopCollectors.module.scss";
 
+import nextId from "react-id-generator";
 
 import chunk from "lodash/chunk";
 import { Grid, FormControl, Select, MenuItem, Container } from "@mui/material";
@@ -7,16 +8,17 @@ import { Grid, FormControl, Select, MenuItem, Container } from "@mui/material";
 import CollectorColumn from "./CollectorColumn";
 
 export default function TopCollectors({ collectors }) {
+    const reactId = nextId();
 
   collectors.map((item, index) => (item.id = index + 1));
 
   return (
     <div className={styles.container}>
-      <Grid className={styles.top} container>
-        <Grid item md={10}>
+      <Container className={styles.top} container>
+        {/* <Grid item md={10}> */}
         <h1 className={styles.heading}>Top Collectors</h1>
-        </Grid>
-        <Grid className={styles.gridSel} item md={2}>
+        {/* </Grid> */}
+        {/* <Grid className={styles.gridSel} item md={2}> */}
         <FormControl sx={{ width: "100%", marginLeft: "auto" }}>
           <Select
             className={styles.selectEl}
@@ -30,12 +32,12 @@ export default function TopCollectors({ collectors }) {
             <MenuItem value={"month"}>This Month</MenuItem>
           </Select>
         </FormControl>
-        </Grid>
-      </Grid>
+        {/* </Grid> */}
+      </Container>
         <Grid className={styles.collectorsGrid} container direction="row">
-          {chunk(collectors, 3).map((collectors) => {
+          {chunk(collectors, 3).map((collectors, i) => {
             return (
-              <Grid item md={3}>
+              <Grid item md={3} key={reactId}>
                 <CollectorColumn items={collectors}></CollectorColumn>
               </Grid>
             );
