86586e75788df4398886a540390025327b26b97d
emo_new_bug <emotupev11@gmail.com>
2023-02-18 16:02:29 +0200

    fix: more stupid changes

--- src/components/collectors/TopCollectors.jsx
+++ src/components/collectors/TopCollectors.jsx
@@ -1,48 +1,30 @@
 import styles from "./TopCollectors.module.scss";
-
-import chunk from "lodash/chunk";
-import { Grid, FormControl, Select, MenuItem, Container } from "@mui/material";
-
+import { Select,MenuItem,Container, Grid } from "@mui/material";
 import CollectorColumn from "./CollectorColumn";
-
-export default function TopCollectors({ collectors = [] }) {
-  collectors.map((item, index) => (item.id = index + 1));
-
+import _ from "lodash"
+export default function TopCollectors({collectors=[]}) {
+    collectors.map((item,index)=>item.id=index+1)
   return (
-    <Container className={styles.container} maxWidth={false}>
-      <div className={styles.top}>
-        <h2 className={styles.heading}>Top Collectors</h2>
-        {/* </Grid> */}
-        {/* <Grid className={styles.gridSel} item md={2}> */}
-        <FormControl sx={{ width: "100%", marginLeft: "auto" }}>
-          <Select
-            className={styles.selectEl}
-            labelId="select"
-            id="select"
-            label="Today"
-            defaultValue="today"
-          >
-            <MenuItem value={"today"}>Today</MenuItem>
-            <MenuItem value={"week"}>This Week</MenuItem>
-            <MenuItem value={"month"}>This Month</MenuItem>
-          </Select>
-        </FormControl>
-      </div>
-      <Container>
-        <Grid container className={styles.collectorGrid} >
-          {chunk(collectors, 3).map((collector, i) => {
-            return (
-              <Grid key={i} item md={3}>
-                <CollectorColumn
-                  className={styles.collectorColumn}
-                  items={collector}
-                  key={i * 3}
-                ></CollectorColumn>
-              </Grid>
-            );
-          })}
-        </Grid>
+    <div >
+        <Container maxWidth={false} className={styles.container}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Top Collectors</h1>
+        <Select
+          defaultValue={"week"}
+          value={"week"}
+          className={styles.select}
+        >
+          <MenuItem value={"week"}>This week</MenuItem>
+          <MenuItem value={"month"}>This month</MenuItem>
+          <MenuItem value={"year"}>This year</MenuItem>
+        </Select>
+      </header>
       </Container>
-    </Container>
+      <Grid container className={styles.collectorGrid}>
+         
+          {_.chunk(collectors,3).map(item=><Grid item><CollectorColumn items={item}/></Grid>)}
+
+      </Grid>
+    </div>
   );
 }
