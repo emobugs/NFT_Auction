57ada3e7db7d505581f64b15ceb403c30380e206
emo_new_bug <emotupev11@gmail.com>
2023-02-19 22:48:04 +0200

    fix: avatar some styling and positioning

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,10 +1,10 @@
 import styles from './Avatar.module.scss'
 
-export default function Avatar({size = 70, url, verified = false}){
+export default function Avatar({size = 50, url, verified = false}){
 
     return(
         <div className={styles.avatar} style={{width: size +'px', height: size +'px'}}>
-            <img src={url+''} alt="" className={styles.image}/>
+            <img src={'./images/avatar.png'} alt="" className={styles.image}/>
             <img src="images/verified.svg" alt="badge" className={styles.badge}/>
         </div>
 
