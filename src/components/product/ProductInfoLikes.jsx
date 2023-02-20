5b5d531d548db6fdd6614a392950a5126c7304ed
emo_new_bug <emotupev11@gmail.com>
2023-02-20 20:14:44 +0200

    feat: add ProductInfoLikes Component

---
+++ src/components/product/ProductInfoLikes.jsx
@@ -0,0 +1,12 @@
+import styles from "./ProductInfoLikes.module.scss"
+
+import millify from "millify"
+import { Chip } from "@mui/material"
+
+export default function ProductInfoLikes({ amount = 0 }) {
+  return (
+    <div className={styles['product-info-likes']}>
+        <Chip className={styles.likes} label={millify(amount)}/>
+    </div>
+  )
+}
