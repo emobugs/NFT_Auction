ae53d7625a0a0b33d4abf0d610776c31796ce687
emo_new_bug <emotupev11@gmail.com>
2023-02-20 21:11:58 +0200

    feat: add variant to chip component

--- src/components/product/ProductInfoLikes.jsx
+++ src/components/product/ProductInfoLikes.jsx
@@ -6,7 +6,7 @@ import { Chip } from "@mui/material"
 export default function ProductInfoLikes({ amount = 0 }) {
   return (
     <div className={styles['product-info-likes']}>
-        <Chip className={styles.likes} label={millify(amount)}/>
+        <Chip className={styles.likes} label={millify(amount)} variant="outlined"/>
     </div>
   )
 }
