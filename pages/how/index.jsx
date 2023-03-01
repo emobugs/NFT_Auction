309264fe22967aa39b07ad9ec21bb153d5f11fb5
emo_new_bug <emotupev11@gmail.com>
2023-03-01 09:35:32 +0200

    fix: hardcode props values

--- pages/how/index.jsx
+++ pages/how/index.jsx
@@ -8,8 +8,11 @@ export default function index() {
   return (
     <div>
         <Header></Header>
-        <Hero text={text}></Hero>
-        <Description text={text} image={image}></Description>
+        <Hero text="How it works"></Hero>
+        <Description text="Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
+    dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
+    Hendrerit parturient habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
+    sodales taciti duis praesent id. Consequat urna vitae morbi nunc congue."></Description>
         <Footer></Footer>
     </div>
   )
