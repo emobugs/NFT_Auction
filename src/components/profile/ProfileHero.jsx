062600725ac957c24b414aa5fb04044feac75170
emo_new_bug <emotupev11@gmail.com>
2023-03-01 14:38:10 +0200

    fix: formatting

--- src/components/profile/ProfileHero.jsx
+++ src/components/profile/ProfileHero.jsx
@@ -5,7 +5,7 @@ import classNames from 'classnames'
 export default function ProfileHero({image}) {
   return (
     <div className={classNames(styles['profile-hero'])}>
-      <img className={classNames(styles['image'])} src={image} alt="profile_hero_image" />
+      <img className={classNames(styles['image'])} src= {image} alt="profile_hero_image" />
     </div>
   )
 }
