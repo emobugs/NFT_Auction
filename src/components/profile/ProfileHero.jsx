39a825eb387c06f565fd7f704e15e3077cfffdde
emo_new_bug <emotupev11@gmail.com>
2023-03-01 12:18:19 +0200

    fix: function Name typo

--- src/components/profile/ProfileHero.jsx
+++ src/components/profile/ProfileHero.jsx
@@ -2,9 +2,9 @@ import styles from "./ProfileHero.module.scss"
 
 import classNames from 'classnames'
 
-export default function Profile({image}) {
+export default function ProfileHero({image}) {
   return (
-    <div className={classNames(styles['profile-hero'])} maxWidth={false}>
+    <div className={classNames(styles['profile-hero'])}>
       <img className={classNames(styles['image'])} src={image} alt="profile_hero_image" />
     </div>
   )
