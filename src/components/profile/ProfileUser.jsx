693558c2f62acff9a5418508284f86f9dbf2d012
emo_new_bug <emotupev11@gmail.com>
2023-03-01 16:58:09 +0200

    feat: add ProfileUser

--- src/components/profile/ProfileUser.jsx
+++ src/components/profile/ProfileUser.jsx
@@ -1,15 +1,15 @@
-import styles from "./ProfileUser.module.scss" 
+import styles from "./ProfileUser.module.scss";
 
-import Avatar from "../avatar/Avatar"
-import classNames from "classnames"
-import { Typography } from "@mui/material"
+import Avatar from "../avatar/Avatar";
+import classNames from "classnames";
+import { Typography } from "@mui/material";
 
-export default function ProfileUser({name, info, avatar, verified}) {
+export default function ProfileUser({ name, info, avatar, verified }) {
   return (
-    <div className={classNames(styles['profile-user'])}>
-        <Avatar size={80} url={avatar} verified={verified}/>
-        <Typography variant="h3">{name}</Typography>
-        <Typography variant="body1">{info}</Typography>
+    <div className={classNames(styles["profile-user"])}>
+      <Avatar size={80} url={avatar} verified={verified} />
+      <Typography variant="h3">{name}</Typography>
+      <Typography variant="body1">{info}</Typography>
     </div>
-  )
+  );
 }
