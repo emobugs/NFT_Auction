d276586b1c1ecd226b841ebcf7f96a7e63726646
emo_new_bug <emotupev11@gmail.com>
2023-03-01 12:10:03 +0200

    fix: replace Container with div

--- src/components/profile/ProfileHero.jsx
+++ src/components/profile/ProfileHero.jsx
@@ -1,12 +1,11 @@
 import styles from "./ProfileHero.module.scss"
 
 import classNames from 'classnames'
-import { Container } from "@mui/material"
 
 export default function Profile({image}) {
   return (
-    <Container className={classNames(styles['profile-hero'])} maxWidth={false}>
+    <div className={classNames(styles['profile-hero'])} maxWidth={false}>
       <img className={classNames(styles['image'])} src={image} alt="profile_hero_image" />
-    </Container>
+    </div>
   )
 }
