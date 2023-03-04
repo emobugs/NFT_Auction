49575f75ab2031c23274e0a62ae1d12a2c00f460
emo_new_bug <emotupev11@gmail.com>
2023-03-04 17:09:00 +0200

    feat: implement profileCollection component

---
+++ src/components/profile/ProfileCollection.jsx
@@ -0,0 +1,23 @@
+import styles from "./ProfileCollection.module.scss"
+
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+
+import ProfileCollectionFilters from "./ProfileCollectionFilters"
+import Card from "../card/Card"
+
+export default function ProfileCollection({user, filter, items}) {
+  return (
+    <div className={styles['profile-collection']}>
+        <Container maxWidth={false}>
+            <Grid container direction='column'>
+                <Grid container item direction='row'>
+                <Grid item xs={3} md={3}><Typography variant='h3'>Collection</Typography></Grid>
+                <Grid item xs={9} md={9}><ProfileCollectionFilters/></Grid>
+                </Grid>
+            </Grid>
+        </Container>
+    </div>
+  )
+}
