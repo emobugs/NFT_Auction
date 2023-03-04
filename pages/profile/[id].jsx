4ee00466a83fbf069f93f8a44ead64c2106cb278
emo_new_bug <emotupev11@gmail.com>
2023-03-05 01:51:30 +0200

    feat: create profile page

---
+++ pages/profile/[id].jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import Header from "../../src/components/header/Header";
+import ProfileHero from "../../src/components/profile/ProfileHero";
+import ProfileUser from "../../src/components/profile/ProfileUser";
+import ProfileCollection from "../../src/components/profile/ProfileCollection";
+import Footer from "../../src/components/footer/Footer";
+
+import profileData from "../../data/profile.json";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+
+export default function profile() {
+  const [profile, setProfile] = useState({});
+
+  const router = useRouter();
+  const id = router.query.id;
+
+  useEffect(() => {
+    // setProfile(profileData);
+    console.log(profileData.nfts);
+  }, [id]);
+  return (
+    <div>
+      <Header />
+      <ProfileHero image={profileData.avatar.url} />
+      <ProfileUser
+        name={profileData.username}
+        avatar={profileData.avatar.url}
+        info={profileData.info}
+        verified={profileData.verified}
+      />
+      <ProfileCollection items={profileData.nfts}/>
+      <Footer />
+    </div>
+  );
+}
