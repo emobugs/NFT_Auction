1c23d01acaa37bb91319abf75a51e30d8aa6b84a
emo_new_bug <emotupev11@gmail.com>
2023-03-04 20:27:04 +0200

    fix: prop name

--- src/components/profile/ProfileCollection.jsx
+++ src/components/profile/ProfileCollection.jsx
@@ -1,23 +1,36 @@
-import styles from "./ProfileCollection.module.scss"
+import styles from "./ProfileCollection.module.scss";
 
-import Container from '@mui/material/Container'
-import Grid from '@mui/material/Grid'
-import Typography from '@mui/material/Typography'
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
-import ProfileCollectionFilters from "./ProfileCollectionFilters"
-import Card from "../card/Card"
+import ProfileCollectionFilters from "./ProfileCollectionFilters";
+import Card from "../card/Card";
 
-export default function ProfileCollection({user, filter, items}) {
+export default function ProfileCollection({ user, filter: filters, items }) {
   return (
-    <div className={styles['profile-collection']}>
-        <Container maxWidth={false}>
-            <Grid container direction='column'>
-                <Grid container item direction='row'>
-                <Grid item xs={3} md={3}><Typography variant='h3'>Collection</Typography></Grid>
-                <Grid item xs={9} md={9}><ProfileCollectionFilters/></Grid>
-                </Grid>
+    <div className={styles["profile-collection"]}>
+      <Container maxWidth={false}>
+        <Grid container direction="column">
+          <Grid container item direction="row">
+            <Grid item xs={3} md={3}>
+              <Typography variant="h3">Collection</Typography>
             </Grid>
-        </Container>
+            <Grid item xs={9} md={9}>
+              <ProfileCollectionFilters filters={filters}/>
+            </Grid>
+          </Grid>
+          <Grid container item>
+            {items.map(item => {
+                return(
+                    <Grid item >
+                        <Card user={user}/>
+                    </Grid>
+                )
+            })}
+          </Grid>
+        </Grid>
+      </Container>
     </div>
-  )
+  );
 }
