21f7191d0308f69c0ed165413a66cdb9eecfbd13
emo_new_bug <emotupev11@gmail.com>
2023-03-05 01:50:23 +0200

    fix: set default prop value

--- src/components/profile/ProfileHero.jsx
+++ src/components/profile/ProfileHero.jsx
@@ -2,7 +2,7 @@ import styles from "./ProfileHero.module.scss"
 
 import classNames from 'classnames'
 
-export default function ProfileHero({image}) {
+export default function ProfileHero({image=""}) {
   return (
     <div className={classNames(styles['profile-hero'])}>
       <img className={classNames(styles['image'])} src= {image} alt="profile_hero_image" />
