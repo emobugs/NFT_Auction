53f713e2fd3a18b5f33794dddb44ac62da910d3b
emo_new_bug <emotupev11@gmail.com>
2023-03-05 04:15:21 +0200

    feat: add Link component to wrap nft and nft owner name

--- src/components/activity/ActivityListItem.jsx
+++ src/components/activity/ActivityListItem.jsx
@@ -1,28 +1,41 @@
-import styles from "./ActivityListItem.module.scss"
+import styles from "./ActivityListItem.module.scss";
 
-import { formatDistance,  parseISO } from "date-fns"
+import { formatDistance, parseISO } from "date-fns";
 
-import Avatar from "../avatar/Avatar"
-import Link from "../link/Link"
-import Stack from '@mui/material/Stack'
+import Avatar from "../avatar/Avatar";
+import Link from "../link/Link";
+import Stack from "@mui/material/Stack";
 
-export default function ActivityListItem({user, created_at, nft, type = "like"}) {
-    const action = () => {
-        if(type === "like"){
-            return "liked";
-        } else if(type === "buy"){
-            return "bought";
-        }
+export default function ActivityListItem({
+  user,
+  created_at,
+  nft,
+  type = "like",
+}) {
+  const action = () => {
+    if (type === "like") {
+      return "liked";
+    } else if (type === "buy") {
+      return "bought";
     }
+  };
   return (
     <div>
-        <Stack className={styles['list-item-stack']} direction="row" spacing={2}>
-            <Avatar url={user.avatar.url} verified={user.verified}/>
-            <Stack className={styles['info-stack']} direction="column">
-                <p className={styles['info-par']}>{`${user.name} ${action()} "${nft.name}" by ${nft.owner.username}`}</p>
-                <p className={styles['info-par']}>{formatDistance(new Date(parseISO(created_at)), new Date(), {addSuffix:true})}</p>
-            </Stack>
+      <Stack className={styles["list-item-stack"]} direction="row" spacing={2}>
+        <Avatar url={user.avatar.url} verified={user.verified} />
+        <Stack className={styles["info-stack"]} direction="column">
+          <p className={styles["info-par"]}>
+            {`${user.name} ${action()}`}
+            <Link href={nft.owner.avatar.url}>"{nft.name}"</Link> by 
+            <Link href={user.avatar.url}>{nft.owner.username}</Link>
+          </p>
+          <p className={styles["info-par"]}>
+            {formatDistance(new Date(parseISO(created_at)), new Date(), {
+              addSuffix: true,
+            })}
+          </p>
         </Stack>
+      </Stack>
     </div>
-  )
+  );
 }
