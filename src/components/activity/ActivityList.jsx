4a0f5a790ad0017dd9480d1f51dec5653b868aa1
emo_new_bug <emotupev11@gmail.com>
2023-03-05 15:56:49 +0200

    fix: typo in the import

--- src/components/activity/ActivityList.jsx
+++ src/components/activity/ActivityList.jsx
@@ -1,4 +1,4 @@
-import styles from "ActivityList.module.scss";
+import styles from "./ActivityList.module.scss"
 
 import { Stack } from "@mui/material";
 import ActivityListItem from "./ActivityListItem";
@@ -7,7 +7,7 @@ export default function ActivityList({ items }) {
   return (
     <div className={styles["activity-list"]}>
       <Stack direction="column" spacing={2}>
-        {items.map((item, i) => {
+        {items?.map((item, i) => {
           const { user, created_at, nft, type } = item;
           return (
             <ActivityListItem
