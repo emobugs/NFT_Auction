b4e7c6da3630a45ac5efbdbab9d3c157060dbe27
emo_new_bug <emotupev11@gmail.com>
2023-03-05 20:37:31 +0200

    feat: create Explore Title component

---
+++ src/components/explore/ExploreTitle.jsx
@@ -0,0 +1,11 @@
+import styles from "./ExploreTitle.module.scss"
+
+import { Typography } from "@mui/material"
+
+export default function ExploreTitle({text}) {
+  return (
+    <div className={styles['explore-title']}>
+        <Typography variant="h1">{text}</Typography>
+    </div>
+  )
+}
