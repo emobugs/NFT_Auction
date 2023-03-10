5c7b97fc9d27cc78a33b3556c61c0b4d89452fc1
emo_new_bug <emotupev11@gmail.com>
2023-03-10 22:26:10 +0200

    feat: add Auctions componenet api calls

--- src/components/auctions/Auctions.jsx
+++ src/components/auctions/Auctions.jsx
@@ -2,46 +2,51 @@ import styles from "./Auctions.module.scss";
 
 import { Container, Grid, FormControl, MenuItem, Select } from "@mui/material";
 import Card from "../card/Card";
-const titles = ['Clock', 'DOGE','BTC', 'Litecoin'];
 
-export default function Auctions({ cards = [] }) {
+import { useState} from "react";
+
+export default function Auctions({ cards = [], filters = [] }) {
+  const [priceFilter, setPriceFilter] = useState(1);
+
+  const handleChange = (e) => {
+    setPriceFilter(e.target.value);
+  };
+
   return (
     <Container className={styles.container} sx={{ width: "100%", margin: 0 }}>
       <div className={styles.top}>
-      <h2 className={styles.heading}>Live Auctions</h2>
-      {/* Select component */}
-      <FormControl sx={{width: '300px' , marginLeft: 'auto'}}>
-        <Select
-          labelId="select"
-          id="select"
-          label="Today"
-        >
-          <MenuItem value={1}>Today</MenuItem>
-          <MenuItem value={2}>This Week</MenuItem>
-          <MenuItem value={3}>This Month</MenuItem>
-        </Select>
-      </FormControl>
+        <h2 className={styles.heading}>Live Auctions</h2>
+        <FormControl sx={{ width: "300px", marginLeft: "auto" }}>
+          <Select defaultValue="" value={priceFilter} onChange={handleChange}>
+            {filters.map((p, i) => {
+              return (
+                <MenuItem label={p.label} value={p.value} key={i}>
+                  {p.label}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
       </div>
       <Container>
-
-      <Grid container display="flex" className={styles["grid-cnt"]}>
-        {cards.map((card, i) => {
-          return (
-            <Grid item md={3} key={i * 3}>
-              <Card
-                key={i}
-                name={titles[i]}
-                mediaUrl={card.mediaUrl}
-                price={card.price}
-                likes={card.likes}
-                currency={card.currency}
-                title={titles[i]}
-                timeLeft="20"
-              ></Card>
-            </Grid>
-          );
-        })}
-      </Grid>
+        <Grid container display="flex" className={styles["grid-cnt"]}>
+          {cards.map((card, i) => {
+            return (
+              <Grid item md={3} key={i * 3}>
+                <Card
+                  key={i}
+                  name={titles[i]}
+                  mediaUrl={card.mediaUrl}
+                  price={card.price}
+                  likes={card.likes}
+                  currency={card.currency}
+                  title={titles[i]}
+                  timeLeft="20"
+                ></Card>
+              </Grid>
+            );
+          })}
+        </Grid>
       </Container>
     </Container>
   );
