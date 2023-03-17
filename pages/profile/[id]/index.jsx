ae91d4c2f230d12d84ac83f3f23a2861dfedba01
emo_new_bug <emotupev11@gmail.com>
2023-03-17 23:03:22 +0200

    fix: remove testing code on profile page

--- pages/profile/[id]/index.jsx
+++ pages/profile/[id]/index.jsx
@@ -7,6 +7,8 @@ import Footer from "../../../src/components/footer/Footer";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 
+import styles from "./index.module.scss"
+
 export default function profile() {
   const [profile, setProfile] = useState({});
   const [profileFilters, setProfileFilters] = useState([]);
@@ -15,22 +17,18 @@ export default function profile() {
   const id = router.query.id;
 
   useEffect(async () => {
-    console.log(id);
     if (id) {
       const response = await fetch(`${process.env.apiUrl}/users/${id}`);
       if (response.ok) {
         const data = await response.json();
-        console.log(data.user.avatar.url);
         setProfile(data.user);
         setProfileFilters(data.filters);
       }
     }
   }, [id]);
 
-  // const renderPage = () => {
-  //   if (profile) {
       return (
-        <div>
+        <div className={styles['wrapper']}>
           <Header />
           {profile.avatar && <ProfileHero image={profile.avatar.url} />}
           <ProfileUser
@@ -43,23 +41,4 @@ export default function profile() {
           <Footer />
         </div>
       );
-    // }
-  // };
-
-  // return (
-  //   <div>
-  //     <Header />
-  //     <ProfileHero image={profile.avatar.url} />
-  //     <ProfileUser
-  //       name={profile.username}
-  //       // avatar={profile.avatar.url}
-  //       info={profile.info}
-  //       verified={profile.verified}
-  //     />
-  //     <ProfileCollection items={profile.nfts} filters={profileFilters} />
-  //     <Footer />
-  //   </div>
-  // );
-
-  // renderPage();
 }
