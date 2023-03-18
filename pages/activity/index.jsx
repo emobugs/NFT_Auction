61de31d6f6036f76b3e5365b09bd1a3b40ed8ec9
emo_new_bug <emotupev11@gmail.com>
2023-03-18 22:21:49 +0200

    fix: api url wrong path

--- pages/activity/index.jsx
+++ pages/activity/index.jsx
@@ -5,28 +5,28 @@ import ActivityFilters from "../../src/components/activity/ActivityFilters";
 import ActivityList from "../../src/components/activity/ActivityList";
 import Footer from "../../src/components/footer/Footer";
 
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 
 export default function index() {
   const [activity, setActivity] = useState([]);
   const [activityFilters, setActivityFilters] = useState([]);
-  const [sort, setSort] = useState('');
-  const [type, setType] = useState('');
+  const [sort, setSort] = useState("");
+  const [type, setType] = useState("");
 
-
-  const sortHandler = (value)=>{
-    console.log(value)
+  const sortHandler = (value) => {
+    console.log(value);
     setSort(value);
-  }
+  };
 
   const typeHandler = (value) => {
-    console.log(value)
+    console.log(value);
     setType(value);
-  }
+  };
 
-  async function getData(){
-   return await (await (await fetch(`${process.env.apiUrl}/explore/?sort=${sort}&type=${type}`)).json());
-
+  async function getData() {
+    return await await (
+      await fetch(`${process.env.apiUrl}/activities/?sort=${sort}&type=${type}`)
+    ).json();
   }
 
   useEffect(async () => {
@@ -35,19 +35,23 @@ export default function index() {
 
     setActivity(data.activities);
     setActivityFilters(data.filters);
-  }, [])
+  }, []);
 
   useEffect(async () => {
-    const result = await getData()
-  }, [sortHandler, typeHandler])
+    const result = await getData();
+    console.log(result);
+  }, [sortHandler, typeHandler]);
 
   return (
     <div className={styles["wrapper"]}>
-      <Header className={styles['header']} />
-        <Hero text="Activity" className={styles["hero"]} />
-      <div className={styles["behind"]}>
-      </div>
-      <ActivityFilters filters={activityFilters} sortHandler={sortHandler} typeHandler={typeHandler}/>
+      <Header className={styles["header"]} />
+      <Hero text="Activity" className={styles["hero"]} />
+      <div className={styles["behind"]}></div>
+      <ActivityFilters
+        filters={activityFilters}
+        sortHandler={sortHandler}
+        typeHandler={typeHandler}
+      />
       <ActivityList items={activity} />
       <Footer />
     </div>
